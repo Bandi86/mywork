bfe50d09fd606ed0037e15b9150e48e80ea6f732
Bandi86 <susutechno@gmail.com>
2023-05-30 10:03:07 +0200

    delete 2 files and update 1 file

--- webshop-next-projekt/app/Nav.tsx
+++ webshop-next-projekt/app/Nav.tsx
@@ -41,10 +41,10 @@ const Nav = () => {
 							<MdAccountCircle className='text-2xl' />
 							Profile
 						</span>
-						<span className='flex flex-row items-center gap-2'>
+						<Link href='login'><span className='flex flex-row items-center gap-2'>
 							<SlLogin className='text-xl' />
 							Login
-						</span>
+						</span></Link>
 						<Link href='/admin'>
 							<span>Admin</span>
 						</Link>
--- webshop-next-projekt/app/layout.tsx
+++ webshop-next-projekt/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import Nav from './Nav';
+import Footer from './Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
